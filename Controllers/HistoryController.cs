c0b36492e90fe30a75aafc3eb90479d511774c48
aron.gamble@sra.org.uk <aron.gamble@sra.org.uk>
2019-11-20 11:34:35 +0000

    Test from VS

--- Controllers/HistoryController.cs
+++ Controllers/HistoryController.cs
@@ -12,34 +12,27 @@ namespace LJGHistoryService.Controllers
     [ApiController]
     public class HistoryController : ControllerBase
     {
+
         private readonly List<EmploymentItem> employmentItems = new List<EmploymentItem>() {
+
                 new EmploymentItem() { Id = 1, CompanyName = "Solicitors Regulation Authority", StartDate = DateTime.Parse("01/11/2018"), EndDate = DateTime.Parse("01/03/2019"), Location = "Birmingham", TypeOfEmployment = EmploymentType.Contract },
                 new EmploymentItem() { Id = 2, CompanyName = "ERGO", StartDate = DateTime.Parse("01/02/2017"), EndDate = DateTime.Parse("01/11/2018"), Location = "Birmingham", TypeOfEmployment = EmploymentType.Contract  }
             };
 
-
-        // GET: api/History
         [HttpGet]
         public IEnumerable<EmploymentItem> Get()
-        {
-            //var employmentItems = new List<EmploymentItem>() {
-            //    new EmploymentItem() { Id = 1, CompanyName = "Solicitors Regulation Authority", StartDate = DateTime.Parse("01/11/2018"), EndDate = DateTime.Parse("01/03/2019"), Location = "Birmingham", TypeOfEmployment = EmploymentType.Contract },
-            //    new EmploymentItem() { Id = 2, CompanyName = "ERGO", StartDate = DateTime.Parse("01/02/2017"), EndDate = DateTime.Parse("01/11/2018"), Location = "Birmingham", TypeOfEmployment = EmploymentType.Contract  }
-            //};
-
+        { 
             return employmentItems;
         }
 
-        // GET: api/History/5
+
         [HttpGet("{id}", Name = "Get")]
         public EmploymentItem Get(int id)
         {
             return employmentItems.Where(x => x.Id == id).Single();
-
-            //return "value";
         }
 
-        // POST: api/History
+
         [HttpPost]
         public void Post([FromBody] string value)
         {
