2897ceb08184457a4f5b74e8037faf1303d097a0
aron.gamble@sra.org.uk <aron.gamble@sra.org.uk>
2019-12-10 16:59:39 +0000

    Added Azure Table Stiorage

--- Controllers/HistoryController.cs
+++ Controllers/HistoryController.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LJGHistoryService.Models;
-using LJGHistoryService.Tables;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace LJGHistoryService.Controllers
 {
@@ -24,32 +20,9 @@ namespace LJGHistoryService.Controllers
             };
 
         [HttpGet]
-        public async Task<IEnumerable<EmploymentItem>> Get()
-        {
-
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=ljgwebsite;AccountKey=4+lge0bw2MN6o9Z4DssravCHaR1ZXuwN+1t26KM8Tb0w+gJeR90iQqFr6HQE/OCG+wjRrzx4+qU0eRLfjgtI6w==;EndpointSuffix=core.windows.net");
-
-            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-
-            CloudTable table = tableClient.GetTableReference("contracts");
-
-            TableQuery<Contract> query = new TableQuery<Contract>()
-                   .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "ljgwebsite"));
-
-            var x = await table.ExecuteQuerySegmentedAsync(query,null);
-
-            var empItems = new List<EmploymentItem>();
-
-            foreach (var y in x)
-            {
-                EmploymentType e = y.TypeOfEmployment == "1" ? EmploymentType.Permanent : EmploymentType.Contract;
-
-                empItems.Add(new EmploymentItem() { CompanyName = y.CompanyName, StartDate = y.StartDate, EndDate = y.EndDate, Id = y.Id, Location = y.Location, TypeOfEmployment = e });
-                
-
-            }
-
-            return empItems;
+        public IEnumerable<EmploymentItem> Get()
+        { 
+            return employmentItems;
         }
 
 
